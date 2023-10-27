0541b8adfd276a64916ed836be83b48f3b5e7736
ibrahimdenizz <dnzibrahim1@gmail.com>
2023-10-27 22:06:06 +0300

    Add copilot plugin

--- lua/custom/configs/keymap.lua
+++ lua/custom/configs/keymap.lua
@@ -1,1 +1,4 @@
 vim.keymap.set('n', '<leader>ef', vim.cmd.Ex, { desc = '[E]xplore [F]ile'})
+
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<Tab><Tab>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
